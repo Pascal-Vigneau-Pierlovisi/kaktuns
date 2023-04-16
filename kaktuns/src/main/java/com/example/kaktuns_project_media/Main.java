298a171880b454421a34360d7bf8003243f0ed0b
dale lynch <ntsn44@gmail.com>
2023-04-16 21:40:05 +0200

    Refactor Main.java

--- kaktuns/src/main/java/com/example/kaktuns_project_media/Main.java
+++ kaktuns/src/main/java/com/example/kaktuns_project_media/Main.java
@@ -1,50 +1,28 @@
 package com.example.kaktuns_project_media;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
-import javafx.scene.layout.TilePane;
-import javafx.scene.media.MediaPlayer;
-import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 import java.util.Objects;
 
 public class Main extends Application {
-    Scene scene;
-
-
     @Override
-    public void start(Stage stage)  {
-        try{
-            Parent root=FXMLLoader.load(Objects.requireNonNull(getClass().getResource("v6.fxml")));
-            scene = new Scene(root);
-
+    public void start(Stage stage) {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("v6.fxml")));
+            Scene scene = new Scene(root);
             stage.setScene(scene);
-
-
-
             stage.setTitle("Kaktuns");
-            Image image=new Image(getClass().getResourceAsStream("logo.png"));
+            Image image = new Image(getClass().getResourceAsStream("logo.png"));
             stage.getIcons().add(image);
-
             stage.show();
-
-
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
+
