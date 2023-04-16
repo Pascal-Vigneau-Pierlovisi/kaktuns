4656c6e9339b919a132352df20d503e792c03a8d
dale lynch <ntsn44@gmail.com>
2023-04-16 22:37:06 +0200

    Create MediaFile.java and MediaFileMp3.java

--- kaktuns/src/main/java/module-info.java
+++ kaktuns/src/main/java/module-info.java
@@ -6,6 +6,9 @@ module com.example.kaktuns_project_media {
     requires org.controlsfx.controls;
 
     requires org.kordamp.bootstrapfx.core;
+    requires org.apache.tika.core;
+    requires java.xml;
+    requires org.apache.tika.parser.audiovideo;
 
     opens com.example.kaktuns_project_media to javafx.fxml;
     exports com.example.kaktuns_project_media;
